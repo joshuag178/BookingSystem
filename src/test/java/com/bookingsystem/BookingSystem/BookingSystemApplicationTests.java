142de154ba06346f97e67e345e852a16419b0fc3
Josue Garcia <josue.garcia@prosone.com>
2024-04-24 23:15:46 -0600

    Creacion del controlador HealthController

---
+++ src/test/java/com/bookingsystem/BookingSystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingsystem.BookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
